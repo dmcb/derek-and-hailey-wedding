dde683d8176d9d7165707d1101a9e521495cddc9
Derek McBurney <d.mcburney@gmail.com>
2012-05-27 02:10:10 -0600

    Resorted script code around Google Analytics inclusion.

--- header.php
+++ header.php
@@ -17,41 +17,20 @@
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
     <script src="scripts/jquery.timers-1.1.2.js"></script>
     <script src="scripts/jquery.scrollorama.js"></script>
-<<<<<<< HEAD
-
-	<!-- Heart effect -->
-	<script type="text/javascript">
-		$(document).ready(function(){
-			$("#tweets").hover(
-			function(){
-				if (!$("#heart").is(":animated")) {
-					$("#heart").everyTime(2, function() {
-						$("#heart").animate({top:"10px"}, 0).animate({opacity:1}, 200).animate({top:"-80px", opacity: 0}, 1000, 'linear').delay(400);
-					});
-				}
-			},
-			function() {
-				$("#heart").stopTime();
-				$("#heart").clearQueue();
-			});
-		});
-	</script>
+    <script src="scripts/script.js"></script>
 
 	<!-- Google Analytics -->
 	<script type="text/javascript">
 		var _gaq = _gaq || [];
 		_gaq.push(['_setAccount', 'UA-32129047-1']);
 		_gaq.push(['_trackPageview']);
-		
+
 		(function() {
 			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
 			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
 			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 		})();
 	</script>
-=======
-    <script src="scripts/script.js"></script>
->>>>>>> Wedding party mad libs added.
   </head>
 
   <body class="scrollblock">
