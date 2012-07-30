4c8f1592fc07e5e184121956aa0338beec04cba4
Derek McBurney <d.mcburney@gmail.com>
2012-07-29 18:05:48 -0600

    Added default configurations for Code Igniter.
    Default configurations for Code Igniter have been added, and git will
    now ignore CI stuff like cache, logs and configurations with passwords.

--- ci_app_2.1.2/config/database.default.php
+++ ci_app_2.1.2/config/database.default.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = 'jays4life';
-$db['default']['database'] = 'rsvp';
+$db['default']['username'] = '';
+$db['default']['password'] = '';
+$db['default']['database'] = '';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
