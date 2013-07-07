6d33d867001e844df4fe706687c6a03e634d1321
Derek McBurney <d.mcburney@gmail.com>
2013-07-07 14:01:49 -0600

    Updated tweets page to use new API 1.1 widgets.
    Twitter feeds had been broken since June 11th!

--- ci_app_2.1.2/views/tweets.php
+++ ci_app_2.1.2/views/tweets.php
@@ -3,62 +3,12 @@
   			<div class="section" id="tweets">
 				<script charset="utf-8" src="http://widgets.twimg.com/j/2/widget.js"></script>
 				<div class="twitter_box half" id="derek_tweets">
-					<script>
-						new TWTR.Widget({
-							version: 2,
-							type: 'profile',
-							rpp: 4,
-							interval: 30000,
-							width: 368,
-							height: 200,
-							theme: {
-								shell: {
-									background: '#dbcbb0',
-									color: '#5a3b08'
-								},
-								tweets: {
-									background: '#f0e7d8',
-									color: '#666666',
-									links: '#f37a7a'
-								}
-							},
-							features: {
-								scrollbar: false,
-								loop: false,
-								live: true,
-								behavior: 'all'
-							}
-						}).render().setUser('derekmcb').start();
-					</script>
+<a class="twitter-timeline" data-dnt="true" href="https://twitter.com/derekmcb" width="380" height="380" data-link-color="#f37a7a" border-color="#dbcbb0" data-widget-id="353961142080663552">Tweets by @derekmcb</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 				</div>
 				<div class="twitter_box" id="hailey_tweets">
-					<script>
-						new TWTR.Widget({
-							version: 2,
-							type: 'profile',
-							rpp: 4,
-							interval: 30000,
-							width: 368,
-							height: 200,
-							theme: {
-								shell: {
-									background: '#dbcbb0',
-									color: '#5a3b08'
-								},
-								tweets: {
-									background: '#f0e7d8',
-									color: '#666666',
-									links: '#f37a7a'
-								}
-							},
-							features: {
-								scrollbar: false,
-								loop: false,
-								live: true,
-								behavior: 'all'
-							}
-						}).render().setUser('haileymcb').start();
-					</script>
+<a class="twitter-timeline" data-dnt="true" href="https://twitter.com/HaileyMcB" width="380" height="380" data-link-color="#f37a7a" border-color="#dbcbb0" data-widget-id="353966419966300161">Tweets by @derekmcb</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 				</div>
 				<div class="twitter_picture">
 					<img src="assets/images/derek_and_hailey_twitter.png" alt="" class="no_border" />
